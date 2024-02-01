83dfd6be0402452f07e663890085058db646bf10
Emine190 <117157285+Emine190@users.noreply.github.com>
2024-02-01 09:32:00 +0100

    Create methylkittest.R

---
+++ methylkittest.R
@@ -0,0 +1,14 @@
+install.package(BiocManger)
+
+
+
+
+library(methylKit)
+file.list=list( system.file("extdata", 
+                            "test1.myCpG.txt", package = "methylKit"),
+                system.file("extdata",
+                            "test2.myCpG.txt", package = "methylKit"),
+                system.file("extdata", 
+                            "control1.myCpG.txt", package = "methylKit"),
+                system.file("extdata", 
+                            "control2.myCpG.txt", package = "methylKit") )
