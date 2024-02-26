dc1e827f9d60f9fd04ac41229f20b25ee8069062
Emil Nelger <117157285+Emine190@users.noreply.github.com>
2024-02-26 13:00:46 +0100

    Update intersect.sh
    made the filepath scrambled

--- intersect.sh
+++ intersect.sh
@@ -22,25 +22,25 @@ bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./r
 # the new intersect used for other things.
 #get the gene names for the regions if there are any 
 
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM1 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R1ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM2 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R2ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM3 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R3ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM4 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R4ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM5 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R5ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM6 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R6ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM7 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R7ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM8 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R8ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM9 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R9ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM10 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R10ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM11 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R11ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM12 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R12ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM13 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R13ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM14 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R14ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM15 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R15ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM16 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R16ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM17 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R17ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM18 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R18ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM19 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R19ASM_Genenames
-bedtools intersect -wao -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/randomwithASM/RandomASM20 -b /home/student/Documents/mykolasandi/Mykolas/Misc/Gene.List.bed > ./RandomASMwithGenenames/R20ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM1 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R1ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM2 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R2ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM3 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R3ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM4 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R4ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM5 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R5ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM6 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R6ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM7 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R7ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM8 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R8ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM9 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R9ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM10 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R10ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM11 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R11ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM12 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R12ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM13 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R13ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM14 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R14ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM15 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R15ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM16 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R16ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM17 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R17ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM18 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R18ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM19 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R19ASM_Genenames
+bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM20 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R20ASM_Genenames
 
 
