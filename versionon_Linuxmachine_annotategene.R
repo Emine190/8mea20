945f84f3b27ea1c06395607ad0f9e3cfb00f6e17
Emil Nelger <117157285+Emine190@users.noreply.github.com>
2024-03-01 10:45:11 +0100

    Update versionon_Linuxmachine_annotategene.R
    updated and extended wiht more functionality

--- versionon_Linuxmachine_annotategene.R
+++ versionon_Linuxmachine_annotategene.R
@@ -30,7 +30,7 @@ cotrolmax <- findMaxima(R10ASM_Genenames.bed, range = 50, metric =(R10ASM_Genena
 controldf <- as.data.frame(R10ASM_Genenames.bed)
 
 
-{ASMdame <- import("~/Documents/mykolasandi/Emil/bedtoolsthings/C2ASMNAMESIMP.bed", format = "bedgraph")
+{ASMdame <- import("~/NAMESIMP.bed", format = "bedgraph")
 Maxima <- findMaxima(ASMdame,  range=10, metric=(ASMdame$score))
 asmdameDF <- as.data.frame(ASMdame)
 peaks <- cbind(asmdameDF,Maxima)}
@@ -60,32 +60,23 @@ ggplot(control1peaks ,aes(x = NA., y = score))+
  # scale_x_discrete(limits=c("GAD2", "SFMBT2", "SNAP25-AS1", "GABRB3"))+
   #geom_jitter()
 
-ggplot(nozeropeaks_control ,aes(x = NA., y = score))+
-  geom_boxplot(notch = FALSE, outlier.alpha = 0.5, outlier.colour = "red")+
-  stat_summary(fun.y = mean, geom = "point",
-               shape = 14, size = 2.5, color = "#FC4E07")+
-  scale_x_discrete(limits=c("ELK3", "GNAS", "SNRPN_intragenic_CpG32", "SNRPN"))+
-  geom_jitter()
-
-
-
 
 #ignore below this as of now 23/2 - 2024
 #Second maxima run
-abovezeropoint1 <- import("~/Documents/mykolasandi/C2.DAME_ASM.Gene.list.above0.1.bedgraph", format = "bedgraph")
-BIngo <- findMaxima(abovezeropoint1,  range=50, metric=runif(length(abovezeropoint1)))
-bingoDF <- as.data.frame(abovezeropoint1)
-peaks <- cbind(bingoDF,BIngo)
+#abovezeropoint1 <- import("~/Documents/mykolasandi/C2.DAME_ASM.Gene.list.above0.1.bedgraph", format = "bedgraph")
+#BIngo <- findMaxima(abovezeropoint1,  range=50, metric=runif(length(abovezeropoint1)))
+#bingoDF <- as.data.frame(abovezeropoint1)
+#peaks <- cbind(bingoDF,BIngo)
 #now remove the false peaks 
 #check the row name to see if it is correct for the data. 
 
-newpeaks <- filter(peaks, BIngo==TRUE)
+#newpeaks <- filter(peaks, BIngo==TRUE)
 
 
-ggplot(no0zeropeaks ,aes(x = NA., y = score))+
-  geom_boxplot(notch = FALSE, outlier.alpha = 0.5, outlier.colour = "red")+
-  stat_summary(fun.y = mean, geom = "point",
-               shape = 14, size = 2.5, color = "#FC4E07")
+#ggplot(no0zeropeaks ,aes(x = NA., y = score))+
+#  geom_boxplot(notch = FALSE, outlier.alpha = 0.5, outlier.colour = "red")+
+#  stat_summary(fun.y = mean, geom = "point",
+#               shape = 14, size = 2.5, color = "#FC4E07")
 #  scale_x_discrete(limits=c("SNRPN", "GNAS", "TUBB8"))
 
 
@@ -108,4 +99,57 @@ huh<-ggplot(Random1ASMdf, aes(x=score)) +
   geom_density()+ xlim(0.25,1)
   huh2 <- huh + geom_density(data = asmdameDF, aes(x=score)) + xlim(0.25,1)
   huh2
+  
+  
+  
+  #start the data analysis of the tuple data 
+  
+  #import the file make sure to Use the RandomASM1 as the control together with the tuple file for impregions. 
+  
+#Tuple imp
+ASMtuples <- import("~/TUPLESimp.bed", format = "bedgraph")
+Maxima <- findMaxima(ASMtuples,  range=10, metric=(ASMtuples$score))
+asmtuplesDF <- as.data.frame(ASMtuples)
+peakstuples <- cbind(asmtuplesDF,Maxima)
+
+tpeaksfilterd <- filter(peaks, Maxima==TRUE)
+tuplepeaksfiltered <- filter(truepeaks, truepeaks$score>0.01)
+
+
+resultmeanstuples<- no0zeropeaks %>%
+  group_by(NA.)%>%
+  dplyr::summarize(Mean = mean(score), .groups = 'drop')
+
+ggplot(tuplepeaksfiltered ,aes(x = NA., y = score))+
+  geom_boxplot(notch = FALSE, outlier.alpha = 0.5, outlier.colour = "red")+
+  stat_summary(fun.y = mean, geom = "point",
+               shape = 14, size = 2.5, color = "#FC4E07")+
+  scale_x_discrete(limits=c("ELK3", "GNAS", "SNRPN_intragenic_CpG32", "SNRPN"))+
+  geom_jitter()
+
+
+#The controle tuple data 
+
+Control1tuples <- import("~/TUPLESRandom1.bed", format = "bedgraph")  
+#have a second control for usage later on if first one looks weird. 
+Control2tuples <- import("~/TUPLESR2.bed", format = "bedgraph")  
+
+Maxima <- findMaxima(Control1tuples,  range=10, metric=(Control1tuples$score))
+C1RtuplesDF <- as.data.frame(Control1tuples)
+C1Rpeakstuples <- cbind(asmtuplesDF,Maxima)
+
+C1Rtpeaksfilterd <- filter(peaks, Maxima==TRUE)
+C1Rtuplepeaksfiltered <- filter(truepeaks, truepeaks$score>0.01)
+
+C1Rtuplesmeans<- C1Rtuplepeaksfiltered %>%
+  group_by(NA.)%>%
+  dplyr::summarize(Mean = mean(score), .groups = 'drop')
+
+
+ggplot(C1Rtuplepeaksfiltered ,aes(x = NA., y = score))+
+  geom_boxplot(notch = FALSE, outlier.alpha = 0.5, outlier.colour = "red")+
+  stat_summary(fun.y = mean, geom = "point",
+               shape = 14, size = 2.5, color = "#FC4E07")#+
+# scale_x_discrete(limits=c("GAD2", "SFMBT2", "SNAP25-AS1", "GABRB3"))+
+#geom_jitter()
   
