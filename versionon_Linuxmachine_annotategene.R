5cdc6ad35df5990f7ce76e78281f8a455694238d
Emil Nelger <117157285+Emine190@users.noreply.github.com>
2024-03-09 13:51:32 +0100

    Update versionon_Linuxmachine_annotategene.R

--- versionon_Linuxmachine_annotategene.R
+++ versionon_Linuxmachine_annotategene.R
@@ -16,8 +16,7 @@ for (i in seq_along(myFiles)){
   bed_data <- import(input_files, format = "begraph")
   results <-findMaxima(bed_data , range = 50, metric = runif(length(input_files)))
   #store results in list
-  
-}
+  }
 
 
 {ControlMaxima <-findMaxima(R1ASM_Genenames.bed,  range=50, metric=runif(length(R1ASM_Genenames.bed)))
@@ -25,7 +24,7 @@ Random1ASMdf <- as.data.frame(R1ASM_Genenames.bed)
 control1peaks <- cbind(Random1ASMdf,ControlMaxima)
 control1truepeaks <- filter(control1peaks, ControlMaxima==TRUE)}
 
-
+#check if needed might need to remove.
 cotrolmax <- findMaxima(R10ASM_Genenames.bed, range = 50, metric =(R10ASM_Genenames.bed$score))
 controldf <- as.data.frame(R10ASM_Genenames.bed)
 
@@ -50,7 +49,7 @@ ggplot(no0zeropeaks ,aes(x = NA., y = score))+
   geom_boxplot(notch = FALSE, outlier.alpha = 0.5, outlier.colour = "red")+
   stat_summary(fun.y = mean, geom = "point",
                shape = 14, size = 2.5, color = "#FC4E07")+
-  scale_x_discrete(limits=c("IGF2_DMR0", "AC090602.2,SNRPN", "GNAS_Ex1A", "GNAS"))+
+  scale_x_discrete(limits=c("IGF2_DMR0", "AC090602.2,SNRPN", "GNAS_XL", "GNAS"))+
   geom_jitter()
 
 ggplot(control1peaks ,aes(x = NA., y = score))+
@@ -60,10 +59,10 @@ ggplot(control1peaks ,aes(x = NA., y = score))+
  # scale_x_discrete(limits=c("GAD2", "SFMBT2", "SNAP25-AS1", "GABRB3"))+
   #geom_jitter()
 
-
+###########################################################################################################
 #ignore below this as of now 23/2 - 2024
 #Second maxima run
-#abovezeropoint1 <- import("~/Documents/mykolasandi/C2.DAME_ASM.Gene.list.above0.1.bedgraph", format = "bedgraph")
+#abovezeropoint1 <- import("~/Documents/DAME_ASM.Gene.list.above0.1.bedgraph", format = "bedgraph")
 #BIngo <- findMaxima(abovezeropoint1,  range=50, metric=runif(length(abovezeropoint1)))
 #bingoDF <- as.data.frame(abovezeropoint1)
 #peaks <- cbind(bingoDF,BIngo)
@@ -78,9 +77,11 @@ ggplot(control1peaks ,aes(x = NA., y = score))+
 #  stat_summary(fun.y = mean, geom = "point",
 #               shape = 14, size = 2.5, color = "#FC4E07")
 #  scale_x_discrete(limits=c("SNRPN", "GNAS", "TUBB8"))
+##########################################################################################################
 
 
-#Need to write a function to get a table of mean of each gene/region in the dataset for comparision. 
+
+#Need to write a function to get a table of mean of each gene/region in the dataset for comparison. 
 library(dplyr)
 resultsofmean <- mean(no0zeropeaks$score, no0zeropeaks$NA.)
 
@@ -92,7 +93,7 @@ cotrol1means<- control1peaks %>%
   group_by(NA.)%>%
   dplyr::summarize(Mean = mean(score), .groups = 'drop')
   
-#This par above now works as intended now just need to visualize it as the table are diffucult to read. 
+#This part above now works as intended now just need to visualize it as the table is difficult to read. 
 
 ############Ideas of how to explain the data. 
 huh<-ggplot(Random1ASMdf, aes(x=score)) +
@@ -100,7 +101,7 @@ huh<-ggplot(Random1ASMdf, aes(x=score)) +
   huh2 <- huh + geom_density(data = asmdameDF, aes(x=score)) + xlim(0.25,1)
   huh2
   
-  
+  ##########################################################################################################
   
   #start the data analysis of the tuple data 
   
