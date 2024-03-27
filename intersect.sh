90183b7d3c2b4b0ef59da5790bc0a6ec78493d81
Emil Nelger <117157285+Emine190@users.noreply.github.com>
2024-03-27 13:34:38 +0100

    Update intersect.sh

--- intersect.sh
+++ intersect.sh
@@ -1,46 +1,17 @@
+#this file needs to be edited to show how it actually was done as the RandomASM approach is scrapped. 
+
+
 bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM1
 bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM2
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM3
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM4
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM5
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM6
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM7
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM8
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM9
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM10
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM11
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM12
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM13
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM14
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM15
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM16
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM17
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM18
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM19
-bedtools intersect -wa -a /goodfilewithASM.bedgraph -b /randomintervals/r1 > ./randomwithASM/randomASM20
 
+#CAN be repeated 18 more time for 20 files for more statisctial singnificance. 
 # the new intersect used for other things.
 #get the gene names for the regions if there are any 
 
 bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM1 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R1ASM_Genenames
 bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM2 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R2ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM3 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R3ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM4 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R4ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM5 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R5ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM6 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R6ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM7 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R7ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM8 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R8ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM9 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R9ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM10 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R10ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM11 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R11ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM12 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R12ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM13 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R13ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM14 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R14ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM15 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R15ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM16 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R16ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM17 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R17ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM18 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R18ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM19 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R19ASM_Genenames
-bedtools intersect -wao -a /home/filepath/randomwithASM/RandomASM20 -b /home/filepath/Gene.List.bed > ./RandomASMwithGenenames/R20ASM_Genenames
+#CAN be repeated 18 more time for 20 files for more statisctial significance. 
+
+# here the newe intersects will be made. 
 
 
