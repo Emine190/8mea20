1cf80f1ab62fd27e8bf957b58a06575a3ed72207
Emil Nelger <117157285+Emine190@users.noreply.github.com>
2024-05-21 09:21:44 +0200

    Update intersectASM_meth_ASR.sh

--- intersectASM_meth_ASR.sh
+++ intersectASM_meth_ASR.sh
@@ -1,8 +1,8 @@
 #Run this for the intersect of ASM MEthylation and ASR. Check the pointers for the files so it lands in the correct folders. 
-bedtools intersect -wa -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/intersect_of_methylation_ASM_ASR/methylation3070_DAMEtupleASM2wa.bedgraph -b /home/student/Documents/mykolasandi/Emil/bedtoolsthings/intersect_of_methylation_ASM_ASR/C2.BS-SNPer.asr.true.al1.bedgraph > ./intersect_Methylation_ASMtuple_ASR/intersect_of_methylation_ASM_ASR/methylation3070_DAMEtupleASM_ASR.bedgraph
+bedtools intersect -wa -a /Emil/intersect_of_methylation_ASM_ASR/methylation3070_DAMEtupleASM2wa.bedgraph -b /Emil/intersect_of_methylation_ASM_ASR/C2.BS-SNPer.asr.true.al1.bedgraph > ./intersect_Methylation_ASMtuple_ASR/intersect_of_methylation_ASM_ASR/methylation3070_DAMEtupleASM_ASR.bedgraph
 
 #
-bedtools intersect -wa -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/intersect_of_methylation_ASM_ASR/methylation3565_DAMEtupleASMwa.bedgraph -b /home/student/Documents/mykolasandi/Emil/bedtoolsthings/intersect_of_methylation_ASM_ASR/C2.BS-SNPer.asr.true.al1.bedgraph > ./intersect_Methylation_ASMtuple_ASR/intersect_of_methylation_ASM_ASR/methylation3565_DAMEtupleASM_ASR.bedgraph
+bedtools intersect -wa -a /Emil/intersect_of_methylation_ASM_ASR/methylation3565_DAMEtupleASMwa.bedgraph -b /Emil/intersect_of_methylation_ASM_ASR/C2.BS-SNPer.asr.true.al1.bedgraph > ./intersect_Methylation_ASMtuple_ASR/intersect_of_methylation_ASM_ASR/methylation3565_DAMEtupleASM_ASR.bedgraph
 
 #
-bedtools intersect -wa -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/intersect_of_methylation_ASM_ASR/methylation4060_DAMEtupleASMwa.bedgraph -b /home/student/Documents/mykolasandi/Emil/bedtoolsthings/intersect_of_methylation_ASM_ASR/C2.BS-SNPer.asr.true.al1.bedgraph > ./iintersect_Methylation_ASMtuple_ASR/ntersect_of_methylation_ASM_ASR/methylation4060_DAMEtupleASM_ASR.bedgraph
+bedtools intersect -wa -a /Emil/intersect_of_methylation_ASM_ASR/methylation4060_DAMEtupleASMwa.bedgraph -b /Emil/intersect_of_methylation_ASM_ASR/C2.BS-SNPer.asr.true.al1.bedgraph > ./iintersect_Methylation_ASMtuple_ASR/ntersect_of_methylation_ASM_ASR/methylation4060_DAMEtupleASM_ASR.bedgraph
