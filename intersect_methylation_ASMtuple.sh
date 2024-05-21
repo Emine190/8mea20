f63d09d8d4bdcbbe5de242f0fcea2b2d298a1cf5
Emil Nelger <117157285+Emine190@users.noreply.github.com>
2024-05-21 09:18:00 +0200

    Update intersect_methylation_ASMtuple.sh

--- intersect_methylation_ASMtuple.sh
+++ intersect_methylation_ASMtuple.sh
@@ -1,13 +1,13 @@
 
 # This will intersect the Methylation and Tuple ASM and put it to in the new folder for easy sorting. 
 
-bedtools intersect -wa -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/intersect_of_Methylation/C2TupleDAME.bedgraph -b /home/student/Documents/mykolasandi/Emil/bedtoolsthings/intersect_of_Methylation/C2.30to70methylation.bedgraph > ./intersect_of_methylation_tupleASM/methylation3565_DAMEtupleASMwa.bedgraph &
+bedtools intersect -wa -a /emil/C2TupleDAME.bedgraph -b /Emil/intersect_of_Methylation/C2.30to70methylation.bedgraph > ./intersect_of_methylation_tupleASM/methylation3565_DAMEtupleASMwa.bedgraph &
 
 #
-bedtools intersect -wa -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/intersect_of_Methylation/C2TupleDAME.bedgraph -b /home/student/Documents/mykolasandi/Emil/bedtoolsthings/intersect_of_Methylation/C2.35to65methylation.bedgraph > ./intersect_of_methylation_tupleASM/methylation3565_DAMEtupleASMwa.bedgraph &
+bedtools intersect -wa -a /emil/C2TupleDAME.bedgraph -b /Emil/intersect_of_Methylation/C2.35to65methylation.bedgraph > ./intersect_of_methylation_tupleASM/methylation3565_DAMEtupleASMwa.bedgraph &
 
 #
-bedtools intersect -wa -a /home/student/Documents/mykolasandi/Emil/bedtoolsthings/C2TupleDAME.bedgraph -b /home/student/Documents/mykolasandi/Emil/bedtoolsthings/intersect_of_Methylation/C2.40to60methylation.bedgraph > ./intersect_of_methylation_tupleASM/methylation3565_DAMEtupleASMwa.bedgraph &
+bedtools intersect -wa -a /emil/C2TupleDAME.bedgraph -b /Emil/intersect_of_Methylation/C2.40to60methylation.bedgraph > ./intersect_of_methylation_tupleASM/methylation3565_DAMEtupleASMwa.bedgraph &
 
 
 
